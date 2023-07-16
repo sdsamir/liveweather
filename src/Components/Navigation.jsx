f54b57661959c2b1f2a5b39e151e7216dbd35736
Samir Kumar Dutta <sdsamirkumardutta@gmail.com>
2023-07-16 16:41:44 +0530

    nav bar added

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -7,18 +7,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Navigation(props) {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
         <Form className="d-flex">
             <InputGroup className="mb-3">
                 <InputGroup.Text>
-                  <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                  <FontAwesomeIcon icon="fa-light fa-magnifying-glass" />
                 </InputGroup.Text>
                 <Form.Control aria-label="Amount (to the nearest dollar)" />
-                <InputGroup.Text>.00</InputGroup.Text>
                 <Button variant="outline-success">Search</Button>
-
             </InputGroup>
         </Form>
       </Container>
